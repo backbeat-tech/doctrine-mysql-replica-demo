73f5dec25cec9154f5d0a64c6490082dcfba1958
Glynn Forrest <glynn@backbeat.tech>
2019-03-27 17:15:43 +0000

    Log which connection is being used

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,26 +8,40 @@ use App\Entity\Item;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ItemRepository;
+use Psr\Log\LoggerInterface;
 
 class AppController extends AbstractController
 {
+    private $em;
+    private $logger;
+
+    public function __construct(EntityManagerInterface $em, LoggerInterface $logger)
+    {
+        $this->em = $em;
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("/")
      */
     public function read(ItemRepository $repo)
     {
-        return $this->json($repo->findBy([]));
+        $items = $repo->findBy([]);
+        $this->logConnectionStatus();
+
+        return $this->json($items);
     }
 
     /**
      * @Route("/insert")
      */
-    public function insert(EntityManagerInterface $em)
+    public function insert()
     {
         $item = new Item();
         $item->setLevel(rand(1, 10000));
-        $em->persist($item);
-        $em->flush();
+        $this->em->persist($item);
+        $this->em->flush();
+        $this->logConnectionStatus();
 
         return $this->json($item, Response::HTTP_CREATED);
     }
@@ -35,12 +49,16 @@ class AppController extends AbstractController
     /**
      * @Route("/update/{id}")
      */
-    public function update(int $id, ItemRepository $repo, EntityManagerInterface $em)
+    public function update(int $id, ItemRepository $repo)
     {
         $item = $repo->find($id);
+        $this->logConnectionStatus();
+
+        // will now switch to primary connection
         $item->setLevel(rand(1, 10000));
-        $em->persist($item);
-        $em->flush();
+        $this->em->persist($item);
+        $this->em->flush();
+        $this->logConnectionStatus();
 
         return $this->json($item);
     }
@@ -48,8 +66,14 @@ class AppController extends AbstractController
     /**
      * @Route("/ping")
      */
-    public function ping(ItemRepository $repo)
+    public function ping()
     {
         return new Response('pong');
     }
+
+    private function logConnectionStatus()
+    {
+        $this->logger->debug(sprintf('Used %s for the previous query.', $this->em->getConnection()->isConnectedToMaster() ? 'PRIMARY' : 'REPLICA'));
+    }
+
 }
